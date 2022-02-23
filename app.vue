c984482a2dfc4ef5b980d6da55d245aa8ff7f55a
BlroG0319 <gyeongtae020319@gmail.com>
2022-02-23 23:16:57 +0900

    전역 스타일 코드 분리 & NuxtLayout 적용

--- app.vue
+++ app.vue
@@ -1,45 +1,13 @@
 <template>
   <div class="app">
-    <NuxtPage class="page" />
+    <NuxtLayout>
+      <NuxtPage class="page" />
+    </NuxtLayout>
   </div>
 </template>
 
 <style lang="scss">
-:root {
-  --pixel-unit: 4px;
-
-  --color-background-normal: rgba(45 50 57);
-  --color-background-dark: rgba(37 40 45);
-  --color-background-darker: rgba(28 30 33);
-  --color-font-strong: rgba(255 255 255 / 1);
-  --color-font-normal: rgba(255 255 255 / 0.75);
-  --color-font-disabled: rgba(255 255 255 / 0.38);
-  --color-font-black-strong: rgba(0 0 0 / 1);
-  --color-font-black-normal: rgba(0 0 0 / 0.75);
-  --color-font-black-disabled: rgba(0 0 0 / 0.38);
-}
-
-* {
-  box-sizing: border-box;
-
-  margin: 0;
-  padding: 0;
-
-  border: none;
-  outline: none;
-
-  -webkit-tap-highlight-color: transparent;
-}
-
-html {
-  background-color: var(--color-background-normal);
-  color: var(--color-font-normal);
-}
-html,
-body,
-#__nuxt {
-  height: 100%;
-}
+@import "~/assets/styles/_root.scss";
 </style>
 
 <style lang="scss" scoped>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,13 @@ export default defineNuxtConfig({
   components: {
     dirs: ["~/components/atoms", "~/components/molecules", "~/components/organisms"],
   },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "~/assets/styles/_variables.scss";`,
+        },
+      },
+    },
+  },
 });
