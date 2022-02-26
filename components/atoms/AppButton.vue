9defef6fa61c3fccf380f2ee6b3b6bf9f7ba3c08
Blro <gyeongtae020319@gmail.com>
2022-02-26 19:20:27 +0900

    feat: AppButton 컴포넌트 제작

---
+++ components/atoms/AppButton.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { RouteLocationRaw } from "vue-router";
+
+interface Props {
+  to?: RouteLocationRaw;
+  href?: string;
+}
+
+const { to, href } = defineProps<Props>();
+
+const tagName = computed(() => {
+  if (to) {
+    return "RouterLink";
+  } else if (href) {
+    return "a";
+  } else {
+    return "button";
+  }
+});
+const attrs = computed(() => {
+  switch (tagName.value) {
+    case "RouterLink":
+      return { to: to };
+    case "a":
+      return {
+        href: href,
+        target: "_blank",
+      };
+    default:
+      return {};
+  }
+});
+</script>
+
+<template>
+  <component
+    v-bind="attrs"
+    :is="tagName"
+    class="app-button"
+    @contextmenu.prevent
+  >
+    <slot />
+  </component>
+</template>
+
+<style lang="scss" scoped>
+.app-button {
+  display: inline-flex;
+  cursor: pointer;
+}
+</style>
