e87c2e882311b96229c02bd84ab8edce05ec6ce1
Blro <gyeongtae020319@gmail.com>
2022-03-01 04:00:26 +0900

    feat: 메뉴 제스처 개선 :D

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,83 +3,113 @@ import TheAppMenu from "~/components/organisms/TheAppMenu.vue";
 
 type TheAppMenuInstance = InstanceType<typeof TheAppMenu>;
 
-const mainElement = ref<HTMLDivElement>(null);
-const mainIsDisabled = ref(false);
-const mainIsDragging = ref(false);
-const mainDragX = ref(0);
-const mainDragProgress = ref(0);
-const menuElement = ref<TheAppMenuInstance>(null);
-const menuIsOpened = ref(false);
-const dragStartPos = { x: 0, y: 0 };
-const dragCurrentPos = { x: 0, y: 0 };
-let framePending = false;
-
-const rootClasses = computed(() => ({
-  "layout--menu-opened": mainIsDisabled.value,
-}));
-const mainClasses = computed(() => ({
-  "layout__main--disabled": menuIsOpened.value,
-  "layout__main--dragging": mainIsDragging.value,
-}));
-const mainStyles = computed<Record<string, string>>(() => ({
-  "--drag-x": mainDragX.value + "px",
-  "--drag-progress": String(mainDragProgress.value),
-}));
-const overlayClasses = computed(() => ({
-  "layout__main__overlay--dragging": mainIsDragging.value,
-}));
 const currentRouteName = computed(() => {
   return useRoute().name;
 });
+const menuElement = ref<TheAppMenuInstance>(null);
+const menuOpened = ref(false);
+const nowGesture = ref(false);
+let pendingUpdateLayout = false;
+let gestureStartX = 0;
+let gestureLastX = 0;
+let gestureMoveX = ref(0);
+let gestureProgress = ref(0);
 
-function handleGestureStart(event: TouchEvent) {
-  if (menuIsOpened.value) return;
-  event.preventDefault();
-  if (event.touches.length > 1) return;
-  mainIsDragging.value = true;
-  dragStartPos.x = event.targetTouches[0].clientX;
-  dragStartPos.y = event.targetTouches[0].clientY;
-  console.log("Start", dragStartPos, dragCurrentPos);
-}
-function handleGestureMove(event: TouchEvent) {
-  if (menuIsOpened.value) return;
-  event.preventDefault();
-  dragCurrentPos.x = event.targetTouches[0].clientX;
-  dragCurrentPos.y = event.targetTouches[0].clientY;
+const layoutStyles = computed<Record<string, string>>(() => ({
+  "--gesture-move-x": gestureMoveX.value + "px",
+  "--gesture-progress": String(gestureProgress.value),
+}));
+const mainClasses = computed(() => ({
+  "layout__main--menu": menuOpened.value,
+  "layout__main--gesture": nowGesture.value,
+  "layout__main--no-gesture": !nowGesture.value,
+}));
 
-  if (framePending) return;
-  framePending = true;
-  requestAnimationFrame(drawGestureFrame);
-  console.log("Move", dragStartPos, dragCurrentPos);
-}
-function handleGestureEnd(event: TouchEvent) {
-  if (menuIsOpened.value) return;
+onMounted(() => {
+  window.addEventListener("pointerdown", handleGestureStart, true);
+  window.addEventListener("pointermove", handleGestureMove, true);
+  window.addEventListener("pointerup", handleGestureEnd, true);
+  window.addEventListener("pointercancel", handleGestureEnd, true);
+});
+onUnmounted(() => {
+  window.removeEventListener("pointerdown", handleGestureStart, true);
+  window.removeEventListener("pointermove", handleGestureMove, true);
+  window.removeEventListener("pointerup", handleGestureEnd, true);
+  window.removeEventListener("pointercancel", handleGestureEnd, true);
+});
+
+function handleGestureStart(event: PointerEvent) {
+  if (event.button !== 0 || event.buttons !== 1) return;
   event.preventDefault();
-  if (mainDragProgress.value > 0.5) {
+  const target = <HTMLElement>event.target;
+  target.setPointerCapture(event.pointerId);
+  nowGesture.value = true;
+  gestureStartX = event.clientX;
+}
+function handleGestureMove(event: PointerEvent) {
+  if (!nowGesture.value) return;
+  event.preventDefault();
+  if (!gestureStartX) return;
+  gestureLastX = event.clientX;
+  if (pendingUpdateLayout) return;
+  pendingUpdateLayout = true;
+  requestAnimationFrame(updateLayout);
+}
+function handleGestureEnd(event: PointerEvent) {
+  if (!nowGesture.value) return;
+  event.preventDefault();
+  if (gestureProgress.value > 0.5) {
     openMenu();
   } else {
     closeMenu();
   }
-  mainIsDragging.value = false;
-  dragStartPos.x = 0;
-  dragStartPos.y = 0;
-  dragCurrentPos.x = 0;
-  dragCurrentPos.y = 0;
-  mainDragX.value = 0;
-  mainDragProgress.value = 0;
-  console.log("End", dragStartPos, dragCurrentPos);
-}
-function drawGestureFrame() {
-  if (!framePending) return;
-  const max = mainElement.value.offsetWidth - 56;
-  let to = dragCurrentPos.x - dragStartPos.x;
-  if (to > max) to = max;
-  else if (to < 0) to = 0;
-  mainDragProgress.value = to / max;
-  mainDragX.value = to;
+  const target = <HTMLElement>event.target;
+  target.releasePointerCapture(event.pointerId);
+  pendingUpdateLayout = false;
+  nowGesture.value = false;
+  gestureStartX = 0;
+  gestureLastX = 0;
+
   nextTick(() => {
-    framePending = false;
+    const moveMax = document.body.offsetWidth - 56;
+    if (menuOpened.value) {
+      gestureMoveX.value = moveMax;
+      gestureProgress.value = 1;
+    } else {
+      gestureMoveX.value = 0;
+      gestureProgress.value = 0;
+    }
   });
+}
+
+function updateLayout() {
+  const moveMax = document.body.offsetWidth - 56;
+  const moveX = gestureLastX - gestureStartX;
+  let to = 0;
+  if (menuOpened.value) {
+    to = moveMax + moveX;
+  } else {
+    to = moveX;
+  }
+  if (to < 0) to = 0;
+  else if (to > moveMax) to = moveMax;
+  gestureMoveX.value = to;
+  gestureProgress.value = to / moveMax;
+  nextTick(() => {
+    pendingUpdateLayout = false;
+  });
+}
+
+function onActivateMenu() {
+  const moveMax = document.body.offsetWidth - 56;
+  menuOpened.value = true;
+  gestureMoveX.value = moveMax;
+  gestureProgress.value = 1;
+}
+function onDeactivateMenu() {
+  menuOpened.value = false;
+  gestureMoveX.value = 0;
+  gestureProgress.value = 0;
 }
 
 function openMenu() {
@@ -91,53 +121,27 @@ function closeMenu() {
 </script>
 
 <template>
-  <div class="layout" :class="rootClasses">
+  <div class="layout" :style="layoutStyles">
     <div class="layout__menu">
       <TheAppMenu
         ref="menuElement"
-        @activate="menuIsOpened = true"
-        @deactivate="menuIsOpened = false"
+        @activate="onActivateMenu"
+        @deactivate="onDeactivateMenu"
       />
     </div>
-    <div
-      class="layout__main"
-      ref="mainElement"
-      :class="mainClasses"
-      :style="mainStyles"
-      @touchstart="handleGestureStart"
-      @touchmove="handleGestureMove"
-      @touchend="handleGestureEnd"
-      @touchcancel="handleGestureEnd"
-    >
+    <div class="layout__main" :class="mainClasses">
       <TheAppHeader @open-menu="openMenu">
         <template #title>{{ currentRouteName }}</template>
       </TheAppHeader>
       <slot />
-      <Transition
-        name="layout__main__overlay"
-        @enter="mainIsDisabled = true"
-        @enter-cancelled="mainIsDisabled = false"
-        @after-leave="mainIsDisabled = false"
-        @leave-cancelled="mainIsDisabled = true"
-      >
-        <div
-          v-if="menuIsOpened || mainIsDragging"
-          class="layout__main__overlay"
-          :class="overlayClasses"
-          @click="closeMenu"
-        />
-      </Transition>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .layout {
+  touch-action: pan-y pinch-zoom;
   height: 100%;
-
-  &--menu-opened {
-    overflow-x: hidden;
-  }
 
   &__menu {
     position: absolute;
@@ -151,22 +155,15 @@ function closeMenu() {
   }
   &__main {
     position: relative;
-    z-index: 1;
 
     height: 100%;
     background-color: $color-background-normal;
 
-    transition: transform 0.2s;
+    &::after {
+      content: "";
+      display: none;
+      opacity: 0;
 
-    &--disabled {
-      transform: translateX(calc(100% - 56px));
-    }
-    &--dragging {
-      transition: none;
-      transform: translateX(var(--drag-x));
-    }
-
-    &__overlay {
       position: absolute;
       top: 0;
       right: 0;
@@ -174,18 +171,29 @@ function closeMenu() {
       left: 0;
 
       background-color: rgba(0 0 0 / 0.2);
-      transition: opacity 0.2s;
+    }
 
-      &--dragging {
-        opacity: var(--drag-progress);
-        transition: none;
+    &--menu {
+      transform: translateX(calc(100% - 56px));
+
+      &::after {
+        display: block;
+        opacity: 1;
       }
+    }
+    &--no-gesture {
+      transition: transform 0.2s;
 
-      &:not(.layout__main__overlay--dragging) {
-        &.layout__main__overlay-enter-from,
-        &.layout__main__overlay-leave-to {
-          opacity: 0;
-        }
+      &::after {
+        transition: opacity 0.2s;
+      }
+    }
+    &--gesture {
+      transform: translateX(calc((100% - 56px) * var(--gesture-progress)));
+
+      &::after {
+        display: block;
+        opacity: var(--gesture-progress);
       }
     }
   }
