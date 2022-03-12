3bfe602903e7858085445a291a596f85d35345c4
Blro <gyeongtae020319@gmail.com>
2022-03-12 20:03:49 +0900

    chore: 코드 정리

--- plugins/bodyScrollLock.client.ts
+++ plugins/bodyScrollLock.client.ts
@@ -1,27 +1,32 @@
 export default defineNuxtPlugin(() => {
   const layout = useLayout();
-  watch(
-    () => layout.value.bodyScrollLock,
-    (lock) => {
-      const target = document.documentElement;
-      if (lock) {
-        layout.value.bodyScrollTop = window.scrollY;
-        layout.value.bodyScrollbarWidth =
-          window.innerWidth - target.clientWidth;
+  watch(() => layout.value.bodyScrollLock, watchBodyScrollLock);
 
-        target.style.position = "fixed";
-        target.style.top = layout.value.bodyScrollTop * -1 + "px";
-        if (layout.value.bodyScrollbarWidth > 0) {
-          target.style.overflowY = "scroll";
-        }
-      } else {
-        target.style.position = "unset";
-        target.style.top = "unset";
-        target.style.overflowY = "auto";
-        window.scrollTo({
-          top: layout.value.bodyScrollTop,
-        });
-      }
+  function watchBodyScrollLock(lock: boolean) {
+    if (lock) {
+      activeBodyScroll();
+    } else {
+      deactiveBodyScroll();
     }
-  );
+  }
+  function activeBodyScroll() {
+    const target = document.documentElement;
+    layout.value.bodyScrollTop = window.scrollY;
+    layout.value.bodyScrollbarWidth = window.innerWidth - target.clientWidth;
+
+    target.style.position = "fixed";
+    target.style.top = layout.value.bodyScrollTop * -1 + "px";
+    if (layout.value.bodyScrollbarWidth > 0) {
+      target.style.overflowY = "scroll";
+    }
+  }
+  function deactiveBodyScroll() {
+    const target = document.documentElement;
+    target.style.position = "unset";
+    target.style.top = "unset";
+    target.style.overflowY = "auto";
+    window.scrollTo({
+      top: layout.value.bodyScrollTop,
+    });
+  }
 });
